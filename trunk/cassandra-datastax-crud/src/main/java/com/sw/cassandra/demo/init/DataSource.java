31c46484a4a7ea03efd1564919f53075cba6b43e
v4u2chat@gmail.com <v4u2chat@gmail.com@e3cd449f-aa12-40e0-0e2b-75b8d29cd937>
2014-04-25 12:19:07 +0000

    Share project "cassandra-datastax-crud" into "https://cassandra-datastax-crud.googlecode.com/svn"

---
+++ trunk/cassandra-datastax-crud/src/main/java/com/sw/cassandra/demo/init/DataSource.java
@@ -0,0 +1,40 @@
+package com.sw.cassandra.demo.init;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.Session;
+
+public class DataSource {
+	
+	private Cluster cluster;
+	private Session session;
+	
+	public void connect(String node) {
+		cluster = Cluster.builder().addContactPoint(node).build();
+		Metadata metadata = cluster.getMetadata();
+		System.out.printf("Connected to cluster: %s\n", metadata.getClusterName());
+		for (Host host : metadata.getAllHosts()) {
+			System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n", host.getDatacenter(), host.getAddress(), host.getRack());
+		}
+		session = cluster.connect();
+	}
+
+	public Cluster getCluster() {
+		return cluster;
+	}
+
+	public void setCluster(Cluster cluster) {
+		this.cluster = cluster;
+	}
+
+	public Session getSession() {
+		return session;
+	}
+
+	public void setSession(Session session) {
+		this.session = session;
+	}
+	
+	
+}
